8303efb58a0f3c49cac87a966610f8928b0076b3
llmlks <lamelkas@gmail.com>
2017-11-06 21:52:11 +0200

    Viikko 2: 3. codacy

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,5 +5,7 @@ import ohtu.ohtuvarasto.Varasto;
 public class Main {
 
     public static void main(String[] args) {
+        Varasto varasto = new Varasto(0);
+        System.out.println(varasto.getSaldo());
     }
 }
